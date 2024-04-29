0df8a0114c8185e2d4b5723af9b422114ef386a4
tzf <tiagozfzfiori@gmail.com>
2024-04-29 11:48:04 -0300

    Created functions that create the grid and the robot

--- Robo/Program.cs
+++ Robo/Program.cs
@@ -10,48 +10,80 @@ namespace Robo
 {
     internal class Program
     {
-        int[,] grid;
+        static int[,] grid;
+        static int x = 0;
+        static int y = 0;
 
         static int[] robot = new int[3];
+        static int robotX = 0;
+        static int robotY = 0;
 
         static void Main(string[] args)
         {
             createMatrix();
         }
 
+        // This functions creates the grid the robot(s) is supposed to explore. Then it creates a robot(or more), sets it's initial coordinates and direction.
         static void createMatrix()
         {
-            int x = 0;
-            int y = 0;
             char robotDirection;
-
-            Console.WriteLine("Now, we will specify the size of the grid\n");
+            Console.WriteLine("Now, we will specify the size of the grid");
             Console.WriteLine("Insert a first value and press enter. Then, insert another value and press enter again\n");
-
             while(x == y)
             {
                 Console.WriteLine("The values can't be equal to each other, since the grid is rectangular");
                 if (int.TryParse(Console.ReadLine(), out x))
                 {
-                    Console.WriteLine($"One of the sides measures {x}\n");
+                    Console.WriteLine($"One of the sides measures {x}");
                 }
                 if (int.TryParse(Console.ReadLine(), out y))
                 {
                     Console.WriteLine($"The second side measures {y}\n");
                 }
             }
-            Console.WriteLine("Please insert the the direction the robot is facing");
-            Console.WriteLine("N - North\n E - East\n S - South\n W - West\n");
-            robotDirection = char.Parse(Console.ReadLine());
-            createRobot(x, y, robotDirection);
+            grid = new int [x, y];
+            int setter = 0;
+            for(int i = 0; i < x; i++)
+            {
+                for(int j = 0; j < y; j++)
+                {
+                    grid[i, j] = setter;
+                    setter = grid[i, j] + 1;
+                }
+            }
+            Console.WriteLine($"The grid the robot will explore measures {grid.Length}\n");
+
+            Console.WriteLine("Please specify the coordinates for the robot now: ");
+            Console.WriteLine("X: ");
+            robotX = int.Parse(Console.ReadLine());
+            Console.WriteLine("Y: ");
+            robotY = int.Parse(Console.ReadLine());
+            if(validateRobot(robotX, robotY, x, y))
+            {
+                createRobot();
+            }
         }
-       
-        static void createRobot(int userInputOne, int userInputTwo, char userInputThree)
+
+        static void createRobot()
         {
-            robot[0] = userInputOne; // First coordinate number
-            robot[1] = userInputTwo; // Second coordinate number
-            robot[2] = userInputThree; // Robot direction
-            // Should we use userInputOne and Two as "global" variables?
+            robot[0] = robotX;
+            robot[1] = robotY;
+            Console.WriteLine("Which direction is the robot facing?: ");
+            robot[2] = char.Parse(Console.ReadLine());
+            Console.WriteLine($"The robot's coordinates are: {x},{y}, it's facing {(char)robot[2]}");
+        }
+
+        static bool validateRobot(int robotX, int robotY, int x, int y)
+        {
+            if(robotX > x)
+            {
+                return false;
+            }
+            if(robotY > y)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
