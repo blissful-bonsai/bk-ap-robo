49fab530caba708985f6b4cd7deba23b1f56abc4
BlissfulBonsai <tiagozfiori@gmail.com>
2024-05-19 02:25:19 -0300

    grid and robots creation

--- Robo/Program.cs
+++ Robo/Program.cs
@@ -2,158 +2,55 @@
 {
     internal class Program
     {
+
+        /// <summary>
+        /// Each robot will be represented by a line
+        /// Each robot will have 4 columns
+        /// x, y, x*y (to represent the coordinate), and angle, which will be used to determine the moving direction
+        /// </summary>
+        static int[,] robots;
+
         static int[,] grid;
-        static int x = 0;
-        static int y = 0;
+        static int gridX;
+        static int gridY;
 
-        static int[] robot = new int[3];
-        static int robotX = 0;
-        static int robotY = 0;
-        static int robotCoordinateValue;
 
-        static int directionIncrement = 90;
         static void Main(string[] args)
         {
-            createMatrix();
-            createRobot();
-            showRobotInfo();
+            createGrid();
+            createRobots();
         }
 
-        // This functions creates the grid the robot(s) is supposed to explore. Then it creates a robot(or more), sets it's initial coordinates and direction.
-        static void createMatrix()
+        /// <summary>
+        /// Here we create the grid, it has to a rectangle
+        /// </summary>
+        static void createGrid()
         {
-            char robotDirection;
-            Console.WriteLine("Now, we will specify the size of the grid");
-            Console.WriteLine("Insert a first value and press enter. Then, insert another value and press enter again\n");
-            while (x == y)
+            Console.WriteLine("What are the grid dimensions?");
+            Console.Write("X: ");
+            gridX = int.Parse(Console.ReadLine());
+            Console.Write("Y: ");
+            gridY = int.Parse(Console.ReadLine());
+            while (gridX == gridY)
             {
-                Console.WriteLine("The values can't be equal to each other, since the grid is rectangular");
-                if (int.TryParse(Console.ReadLine(), out x))
-                {
-                    Console.WriteLine($"One of the sides measures {x}");
-                }
-                if (int.TryParse(Console.ReadLine(), out y))
-                {
-                    Console.WriteLine($"The second side measures {y}\n");
-                }
+                Console.WriteLine("It's a rectangle, the sizes can't be equal to each other");
+                Console.Write("X: ");
+                gridX = int.Parse(Console.ReadLine());
+                Console.Write("Y: ");
+                gridY = int.Parse(Console.ReadLine());
             }
-            grid = new int[x, y];
-            int setter = 0;
-            for (int i = 0; i < x; i++)
-            {
-                for (int j = 0; j < y; j++)
-                {
-                    grid[i, j] = setter;
-                    setter = grid[i, j] + 1;
-                }
-            }
-            Console.WriteLine($"The grid the robot will explore measures {grid.Length}\n");
-            Console.WriteLine($"{grid[1, 1]}");
+            grid = new int[gridX, gridY];
         }
-
-        static void createRobot()
+        /// <summary>
+        /// Here we create the robots, each robot is represented by a line.
+        /// Each line has 4 columns: x, y, x*y and angle
+        /// </summary>
+        static void createRobots()
         {
-            Console.WriteLine("Inform the x and y coordinates: ");
-            robotX = int.Parse(Console.ReadLine());
-            robotY = int.Parse(Console.ReadLine());
-            Console.WriteLine("Which direction is the robot facing? East-E, North-N, West-W or South-S?");
-            robot[2] = char.Parse(Console.ReadLine());
-            if (robot[2] == 'E')
-            {
-                robot[2] = 0;
-            }
-            else if (robot[2] == 'N')
-            {
-                robot[2] = 90;
-            }
-            else if (robot[2] == 'W')
-            {
-                robot[2] = 180;
-            }
-            else
-            {
-                robot[2] = 270;
-            }
-            Console.WriteLine($"The robot is on the {robotX},{robotY} coordinate!");
-            Console.WriteLine($"{grid[robotX, robotY]}");
-            Console.WriteLine($"The robot is facing, numerically: {robot[2]}");
-            receiveOrders();
-        }
-
-        static void receiveOrders()
-        {
-            char previousDirection = (char)robot[2];
-            Console.WriteLine("L - Turn 90 degrees to the left. R - Turn 90 deegres to the right. M - Move one point forward on the grid");
-            Console.WriteLine("Specify a list of movements like LRLRLRLRM, the robot will complete each of them in sequence");
-            string movementList = Console.ReadLine();
-            for (int i = 0; i < movementList.Length; i++)
-            {
-                char direction = movementList[i];
-                switch (direction)
-                {
-                    case 'L':
-                        turnLeft();
-                        break;
-
-                    case 'R':
-                        turnRight();
-                        break;
-
-                    case 'M':
-                        move();
-                        break;
-                }
-            }
-        }
-
-        static void checkFullCircle()
-        {
-            if (robot[2] == 360)
-            {
-                robot[2] = 0;
-            }
-        }
-        static void turnLeft()
-        {
-            robot[2] += directionIncrement;
-            checkFullCircle();
-        }
-
-        static void turnRight()
-        {
-            robot[2] -= directionIncrement;
-            if (robot[2] < 0)
-            {
-                robot[2] = 270;
-            }
-        }
-
-
-        static void move()
-        {
-            switch (robot[2])
-            {
-                case 0: // East
-                    robotX += 1;
-                    break;
-
-                case 90: // North
-                    robotY += 1;
-                    break;
-
-                case 180:
-                    robotX -= 1;
-                    break;
-
-                case 270:
-                    robotY -= 1;
-                    break;
-            }
-        }
-
-        static void showRobotInfo()
-        {
-            Console.WriteLine($"{robotX}, {robotY}, {robot[2]}");
+            Console.WriteLine("How many robots do you want?");
+            int numberOfRobots = int.Parse(Console.ReadLine());
+            robots = new int[numberOfRobots, 4];
+            //Console.WriteLine(robots.Length); // Testing purposes
         }
 
 
